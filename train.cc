32c83ab3f9b9766a9f59519b87392e8466839563
bryqnt <123854285+bryqnt@users.noreply.github.com>
2024-01-12 22:17:23 -0800

    train.cc

---
+++ train.cc
@@ -0,0 +1,21 @@
+// Bryant Huynh
+// CPSC 121L-01
+// 11/27/2023
+// 22bhuynh2@csu.fullerton.edu
+// @bryqnt
+//
+// Lab 11-2
+
+#include "train.h"
+
+// ========================= YOUR CODE HERE =========================
+// This implementation file (train.cc) is where you should implement
+// the member functions declared in the header (train.h), only
+// if you didn't implement them inline within train.h.
+//
+// Remember to specify the name of the class with :: in this format:
+//     <return type> MyClassName::MyFunction() {
+//        ...
+//     }
+// to tell the compiler that each function belongs to the Train class.
+// ===================================================================
